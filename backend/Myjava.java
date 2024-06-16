96d00854281ee385ef6f0b84070755647a9a616d
cmchandru <cmcchandru@gmail.com>
2024-06-16 16:21:34 +0530

    This is my first commit..

---
+++ backend/Myjava.java
@@ -0,0 +1,14 @@
+package mypack;
+
+public class Myjava{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0 ; i<=5 ;i++) {
+			for(int j=0 ;j<=5;j++) {
+				System.out.println(i+j);
+			}
+		}
+	}
+
+}
